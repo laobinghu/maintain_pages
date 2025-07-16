173753b7be31b7fd043dc175e193253904b476f2
laobinghu <923190468@qq.com>
2025-07-16 12:10:45 +0800

    "添加Vercel适配器配置并更新.gitignore"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://blog.laobinghu.top",
+  base: "/",
+  trailingSlash: "always",
+  output: "static",
+  compressHTML: true,
+
+  devToolbar: {
+    enabled: false,
+  },
+
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy:"viewport"
+  },
+
+  adapter: vercel({webAnalytics: {
+      enabled: true,
+    },
+    imageService: true,
+    devImageService: 'sharp',
+    isr: {
+      // 在首次请求时缓存所有页面，并保存 1 天
+      expiration: 60 * 60 * 24,
+    },
+
+  }),
+});
